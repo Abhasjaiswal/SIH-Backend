b50816772848423547fd8886842667db03d26f67
Abhasjaiswal <abhas.jaiswal0907@gmail.com>
2024-09-12 23:35:59 +0530

    update server

--- server/models/User.js
+++ server/models/User.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const User = require("../models/User");
-const bcrypt = require("bcryptjs");
+// const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const dotenv = require("dotenv");
 
